c40f8d265a44afcdce980a54a98a219cb6068a17
minhthi1310 <150514835+minhthi1310@users.noreply.github.com>
2023-11-11 22:16:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,15 @@ Created on Sat Nov 11 19:54:49 2023
 
 @author: Windows 10
 """
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    author="DSSS",
+    author_email="thi.m.phamqfau.de",
+    packages=find_packages(),
+    install_requires=[],
+)
 
