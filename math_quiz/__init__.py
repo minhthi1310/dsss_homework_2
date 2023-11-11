f3828f8327766e7f071b9e43f200fa23cf4a40df
minhthi1310 <minhthipham13gmail.com>
2023-11-11 19:57:12 +0100

    missing files for pip installation added

---
+++ math_quiz/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 19:52:54 2023
+
+@author: Windows 10
+"""
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 19:54:49 2023
+
+@author: Windows 10
+"""
+
